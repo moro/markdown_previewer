61eb880f9ae6ab987452a400fa772cacede3ae2d
moro <moronatural@gmail.com>
2012-11-20 18:39:56 +0900

    extracted from an app.

--- lib/markdown_previewer.rb
+++ lib/markdown_previewer.rb
@@ -1,5 +1,39 @@
-require "markdown_previewer/version"
+require 'redcarpet'
+require 'markdown_previewer/version'
 
 module MarkdownPreviewer
-  # Your code goes here...
+  class << self
+    def renderer=(renderer)
+      @renderer = normalize_renderer(renderer)
+    end
+
+    def renderer
+      @renderer
+    end
+
+    private
+
+    def normalize_renderer(r)
+      return r if r.respond_to?(:call)
+      return lambda {|src| r.render(src) } if r.respond_to?(:render)
+
+      raise ArgumentError, 'renderer should respond to either call or render'
+    end
+  end
+
+  self.renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+
+  class App
+    def call(env)
+      req = Rack::Request.new(env)
+
+      [200, {'Content-Type' => 'text/html'}, [render(req.params['src'])]]
+    end
+
+    private
+
+    def render(src)
+      ::MarkdownPreviewer.renderer.call(src)
+    end
+  end
 end
