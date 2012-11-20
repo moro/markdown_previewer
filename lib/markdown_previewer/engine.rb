29886bb74e012e1863c6677249563bb97f156f8c
moro <moronatural@gmail.com>
2012-11-20 18:54:32 +0900

    renamed

--- lib/markdown_previewer/engine.rb
+++ lib/markdown_previewer/engine.rb
@@ -1,5 +1,5 @@
 module MarkdownPreviewer
-  class Railtie < ::Rails::Railtie #:nodoc:
+  class Engine < ::Rails::Engine #:nodoc:
     initializer 'Markodwn Previewer' do |app|
        app.config.assets.precompile += ['markdown_previewer.js']
     end
